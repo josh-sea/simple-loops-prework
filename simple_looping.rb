e44e807e1fc2c25aa85b6116c9617dc1c3e44812
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-10-29 03:11:15 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,6 +12,7 @@ def loop_iterator(number_of_times)
   loop do
     counter += 1
     phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
     break if number_of_times == counter
   end
 end
